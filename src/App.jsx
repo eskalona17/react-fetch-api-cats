73750d83ef4dc87402543b939ca141f4a1650a98
Fernando <fescalona@immediait.com>
2023-09-02 11:59:36 +0200

    api call with useEffect hook and render state

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+
+const CAT_ENDPINT_RANDOM_FACT = "https://catfact.ninja/fact";
+
+const CAT_PREFIX_IMAGE_URL = "https://cataas.com";
 
 function App() {
+  const [fact, setFact] = useState();
 
+  useEffect(() => {
+    fetch(CAT_ENDPINT_RANDOM_FACT)
+      .then((res) => res.json())
+      .then((data) => {
+        const { fact } = data;
+        setFact(fact);
+      });
+  }, []);
+  
   return (
-    <>
-      <h1>hello world</h1>
-    </>
-  )
+    <main>
+      <h1>Cats App</h1>
+      {fact}
+    </main>
+  );
 }
 
-export default App
+export default App;
