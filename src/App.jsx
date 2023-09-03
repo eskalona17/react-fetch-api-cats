9954cee139f1e27a2dbe4b1095c22fbce53586dd
Fernando <fescalona@immediait.com>
2023-09-03 12:27:48 +0200

    add cat image using another endpoint

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,47 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 
-const CAT_ENDPINT_RANDOM_FACT = "https://catfact.ninja/fact";
+const CAT_ENDPOINT_RANDOM_FACT = "https://catfact.ninja/fact";
 
-const CAT_PREFIX_IMAGE_URL = "https://cataas.com";
+const CAT_ENDPOINT_IMAGE_URL = "https://cataas.com/cat"; // You need to specify a cat name or ID here
+
 
 function App() {
   const [fact, setFact] = useState();
+  const[image, setImage] = useState();
 
   useEffect(() => {
-    fetch(CAT_ENDPINT_RANDOM_FACT)
+    fetch(CAT_ENDPOINT_RANDOM_FACT)
       .then((res) => res.json())
       .then((data) => {
         const { fact } = data;
         setFact(fact);
       });
+
+      fetch(CAT_ENDPOINT_IMAGE_URL)
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return res.blob(); // Fetch the image as a blob
+      })
+      .then((blob) => {
+        const imageUrl = URL.createObjectURL(blob);
+        setImage(imageUrl);
+      })
+      .catch((error) => {
+        console.error("Error fetching image:", error);
+      });
+
   }, []);
 
   return (
     <main>
       <h1>Cats App</h1>
       {fact && <p>{fact}</p>}
+      {image && (
+        <img src={image} />
+      )}
     </main>
   );
 }
